4780e6d1bac3ca2c1bedc4f495ceab5e9354caf4
Mauricio Rivero <rivero.mauricio@gmail.com>
2023-03-01 23:34:58 -0500

    created the funtions and variables js files for the first day

---
+++ Day1/funciones.js
@@ -0,0 +1,13 @@
+//Esta es la forma de declarar una funcion para luego ser llamada
+
+function mostrarNombre(nombre){
+    //Lo que está dentro de los parentesis anteriores es el argumento o parametro de entrada
+    //lo que está entre las dos llaves son las instrucciones de la función.
+    console.log("El nombre es: "+nombre);
+}
+
+function mostrarNombreEdad(nombre, edad){
+    console.log("El nombre es: "+nombre);
+    console.log("La edad es: "+edad);
+}
+
---
+++ Day1/variables.js
@@ -0,0 +1,18 @@
+//Declaracion de variables 
+var edad;
+let nombre;
+
+//inicialización de variables
+edad = 8;
+nombre = "Mauricio";
+
+//Decalaración e inicialización de variables
+var usuario = "mauro@mrd2.co";
+let fecha = "2023-01-01";
+const temperatura = 30; //las constantes se deben declarar e inicializar
+
+/*
+la palabra reservada var se usa comunmente para declarar variables globales
+la palabra reservada let se usa comunmente para declarar variables locales
+*/
+
