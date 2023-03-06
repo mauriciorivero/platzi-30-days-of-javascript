2ee74b3ef03019c3c38522e92abb3c74f8d71688
Mauricio Rivero <rivero.mauricio@gmail.com>
2023-03-06 17:03:43 -0500

    added the file manipulacionStrings with strings methods

---
+++ Day1/manipulacionSrings.js
@@ -0,0 +1,15 @@
+var palabraActividad = "Actividad de motricidad fina 1";
+console.log("La cadena de texto es: "+palabraActividad);
+
+//metodos para manipular este string
+//metodo lenght es para saber el total de caracteres
+console.log("El total de letras de esta cadena es: "+palabraActividad.length);
+
+//metodo para poner todo en mayusculas
+console.log("La misma palabra toda en mayusculas: "+palabraActividad.toUpperCase());
+
+//metodo para poner todo en minusculas
+console.log("La misma palabra toda en mayúsculas: "+palabraActividad.toLowerCase());
+
+//Metodo para devolver solo una parte del string
+console.log("Los caracteres que están entre el 5 y el 16 son: "+palabraActividad.substring(5,16));
